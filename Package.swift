b9a3d750cdf4e0d2e0eefbdff1eb07ab31e7bd5b
consuelita <adelita@operatorfoundation.org>
2021-10-11 17:40:29 -0500

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "InternetProtocols",
     platforms: [
-        .macOS(.v11)
+        .macOS(.v10_15)
     ],
     products: [
         .library(
@@ -14,8 +14,8 @@ let package = Package(
             targets: ["InternetProtocols"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/OperatorFoundation/Bits.git", from: "2.0.0"),
-        .package(url: "https://github.com/OperatorFoundation/Datable.git", from: "3.1.0"),
+        .package(url: "https://github.com/OperatorFoundation/Bits.git", from: "2.0.1"),
+        .package(url: "https://github.com/OperatorFoundation/Datable.git", from: "3.1.1"),
         .package(url: "https://github.com/OperatorFoundation/SwiftPCAP.git", from: "1.3.1"),
     ],
     targets: [
