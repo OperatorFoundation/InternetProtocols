446dbf2f4353cb1eaba9bf7c3df8e21035e67e92
consuelita <adelita@operatorfoundation.org>
2021-11-23 13:34:22 -0600

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/OperatorFoundation/Bits.git", from: "2.0.1"),
-        .package(url: "https://github.com/OperatorFoundation/Datable.git", from: "3.1.1"),
+        .package(url: "https://github.com/OperatorFoundation/Datable.git", from: "3.1.3"),
         .package(url: "https://github.com/OperatorFoundation/SwiftPCAP.git", from: "1.3.1"),
     ],
     targets: [
